b5f8dac289a129e94046760a205f16524472d639
Petro <petrotymchuck@gmail.com>
2025-11-29 21:06:54 +0200

    lesson work

--- src/main/java/org/example/repository/ICityRepository.java
+++ src/main/java/org/example/repository/ICityRepository.java
@@ -1,12 +1,11 @@
 package org.example.repository;
 
 import org.example.entities.location.CityEntity;
-import org.example.entities.location.CountryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ICityRepository extends JpaRepository<CityEntity, Long> {
-    Optional<CountryEntity> findBySlug(String slug);
+    Optional<CityEntity> findBySlug(String slug);
     boolean existsBySlug(String slug);
 }
