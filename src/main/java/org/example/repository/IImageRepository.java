d6c468b7470484b9ea3d2def16fb457514fb337e
Petro <petrotymchuck@gmail.com>
2025-12-01 18:19:56 +0200

    add Scheduled

--- src/main/java/org/example/repository/IImageRepository.java
+++ src/main/java/org/example/repository/IImageRepository.java
@@ -4,5 +4,8 @@ package org.example.repository;
 import org.example.entities.common.ImageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IImageRepository extends JpaRepository<ImageEntity, Long> {
+    List<ImageEntity> findByCityIsNull();
 }
